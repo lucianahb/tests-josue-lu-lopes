70a50561eb0fd5fc3cabd4f019ce5363540d1035
josue-avila <josue.avila@olist.com>
2021-01-29 16:45:56 -0300

    refactoring ctegory model tests: using pytest modules and decorators instead sad asserts

--- tests/test_category_model.py
+++ tests/test_category_model.py
@@ -1,43 +1,52 @@
 from models.categories import Category
 from models.base_model import BaseModel
+import pytest
 
-class TestCategoryModel:
-    def test_name_isinstance_str(self):
-        try:
-            Category(None, 'test description')
-            raise NotImplementedError('Not implemented error')
-        except Exception as error:
-            assert isinstance(error, TypeError)
+
+class TestModels:
+    @pytest.mark.parametrize("name, description", [
+                                ('test name', 'test description'),
+                                ('t', ''),
+                                ('t'*100, 't'*200)
+                            ])
+    def test_category_instance(self, name, description):
+        test_instance = Category(name, description)
+        assert isinstance(test_instance, Category)
+        assert isinstance(test_instance, BaseModel)
+
+    def test_category_args(self):
+        name = 'test name'
+        description = 'test description'
+        test_instance = Category(name, description)
+        assert test_instance.name is name
+        assert test_instance.description is description
+
+    @pytest.mark.parametrize("name", [
+                                        None, 5, 5.0, [1, 2, 3],
+                                        ('a', 'b', 'c'),
+                                        Category('name', 'description')
+                                    ])
+    def test_name_not_instance_str(self, name):
+        with pytest.raises(TypeError):
+            Category(name, 'test description')
 
     def test_name_blank_spaces(self):
-        try:
+        with pytest.raises(ValueError):
             Category(' ', 'test description')
-            raise NotImplementedError('Not implemented error')
-        except Exception as error:
-            assert isinstance(error, ValueError)
 
     def test_name_too_big(self):
-        try:
+        with pytest.raises(ValueError):
             Category('test name'*100, 'test description')
-            raise NotImplementedError('Not implemented error')
-        except Exception as error:
-            assert isinstance(error, ValueError)
 
-    def test_description_isinstance_str(self):
-        try:
-            Category('test name', None)
-            raise NotImplementedError('Not implemented error')
-        except Exception as error:
-            assert isinstance(error, TypeError)
+    @pytest.mark.parametrize("description", [
+                                        None, 5, 5.0, [1, 2, 3],
+                                        ('a', 'b', 'c'),
+                                        Category('name', 'description')
+                                    ])
+    def test_description_not_instance_str(self, description):
+        with pytest.raises(TypeError):
+            Category('test name', description)
 
     def test_description_too_big(self):
-        try:
-            Category('test name'*100, 'test description')
-            raise NotImplementedError('Not implemented error')
-        except Exception as error:
-            assert isinstance(error, ValueError)
-
-    def test_category_instance(self):
-        category = Category('test name', 'test description')
-        assert isinstance(category, Category)
-        assert isinstance(category, BaseModel)
+        with pytest.raises(ValueError):
+            Category('test name', 'test description'*200)
