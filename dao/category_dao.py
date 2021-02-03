eb46017a3728584ae05311d312d593455e2338a4
Matheus B. Lopes <matheus.bachiste@olist.com>
2021-02-03 13:58:38 -0300

    Hotfix

--- dao/category_dao.py
+++ dao/category_dao.py
@@ -12,14 +12,14 @@ class CategoryDao:
     def read_by_id(self, id_: int) -> Category:
         if isinstance(id_, int):
             with Session() as session:
-                result = session.query(self.__type_model).filter_by(id_=id_).first()
+                result = session.query(Category).filter_by(id=id_).first()
             return result
         else:
             raise TypeError('Id mut be integer')
 
     def read_all(self) -> list:
         with Session() as session:
-            result = session.query(self.__type_model).order_by('id').all()
+            result = session.query(Category).order_by('id').all()
         return result
 
     def delete(self, model: Category) -> None:
--- dao/session.py
+++ dao/session.py
@@ -10,12 +10,12 @@ class Session:
         dbname = 'topskills17'
         self.__conn_string = f'{connector}://{user}:{password}@{host}:3306/{dbname}'
 
-        def __enter__(self):
-            self.__engine = create_engine(self.__conn_string)
-            Session = sessionmaker(self.__engine)
-            self.__session = Session()
-            return self.__session
+    def __enter__(self):
+        self.__engine = create_engine(self.__conn_string)
+        Session = sessionmaker(self.__engine)
+        self.__session = Session()
+        return self.__session
 
-        def __exit__(self, type, value, traceback):
-            self.__session.close()
-            self.__engine.dispose()
+    def __exit__(self, type, value, traceback):
+        self.__session.close()
+        self.__engine.dispose()
